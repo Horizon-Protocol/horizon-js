8ffe35bf27b062a8a818869e7a971acdefcfa347
lxz <bithub@github.com>
2020-12-18 20:38:13 +0800

    Regenerate js bundle according contract change.

--- src/contracts/testnet/index.js
+++ src/contracts/testnet/index.js
@@ -8,6 +8,7 @@ import SynthetixEscrow from './SynthetixEscrow';
 import SynthetixState from './SynthetixState';
 import RewardEscrow from './RewardEscrow';
 import DappMaintenance from './DappMaintenance';
+import Exchanger from './Exchanger';
 import EtherCollateral from './EtherCollateral';
 import Issuer from './Issuer';
 import AddressResolver from './AddressResolver';
@@ -65,6 +66,7 @@ export default {
   SynthetixState,
   RewardEscrow,
   DappMaintenance,
+  Exchanger,
   EtherCollateral,
   Issuer,
   AddressResolver,
